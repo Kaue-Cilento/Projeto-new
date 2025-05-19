2d7d2fdb02dadd7dbc043b3936743148ca9df0f7
Danilo <dev02@senhorcaixa.com.br>
2025-05-19 18:09:20 -0300

    ProjetoPronto

---
+++ exemplo.js
@@ -0,0 +1,7 @@
+let barraDeChocolate = 0; 
+let desejo = false; 
+while(!desejo){
+   barraDeChocolate++;
+   desejo=barraDeChocolate < 5;
+}
+console.log('Danilo_Apelão comeu ${barraDeChocolate} barra de chcolate');
